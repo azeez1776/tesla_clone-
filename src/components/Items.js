3fde12fc53b4df9d6e17ea71240c29cbb2c5b65f
azeez1776 <aboud3am81@gmail.com>
2021-04-28 02:03:50 +0300

    tesla-clone

---
+++ src/components/Items.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Items.css';
+import Button from './Button';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+function Items({ title, desc, backgroundImg, leftBtnText, leftBtnLink, rightBtnText, rightBtnLink, twoButton, first }) {
+    return (
+        <div className="items" style={{
+            backgroundImage: `url(${backgroundImg})`,
+        }
+        }>
+            <div className="items_container">
+                <div className="items_text">
+                    <p>{title}</p>
+                    <div className="items_textDesc">
+                        {desc}
+                    </div>
+                </div>
+                <div className="items_lowerThird">
+                    <div className="items_buttons">
+                        <Button imp="primary" text={leftBtnText} link={leftBtnLink} />
+                        {twoButton &&
+                            (<Button imp="secondary" text={rightBtnText} link={rightBtnLink} />)
+                        }
+
+                    </div>
+                    {first &&
+                        (<div className="item_expand">
+                            <ExpandMoreIcon />
+                        </div>)
+                    }
+
+                </div>
+            </div>
+
+        </div >
+    )
+}
+
+export default Items
